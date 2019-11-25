835f9499200f27ce927db5058beeeb3d71438ddf
Mayank Sharma <mayanksharma78613@gmail.com>
2019-11-25 07:46:59 +0530

    Delete online.php

--- online.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require 'commonfb.php';
-$sql="select online.userid,CONCAT(users.first,' ',users.last)as name from online,friends,users where online.userid=users.id and friends.friend_id=users.id and friends.request='accept' and friends.fid=".$_SESSION['id'];
-$result=mysqli_query($con,$sql);$json_array=array();
-while($row=mysqli_fetch_assoc($result)){
-    $json_array[]=$row;
-    }
-     print_r(json_encode($json_array));?>
