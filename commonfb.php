625c4892204443f2f7eb0e31f4de91f0ac878873
Mayank Sharma <mayanksharma78613@gmail.com>
2019-11-25 07:45:52 +0530

    Delete commonfb.php

--- commonfb.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-$dbservername="localhost";
-$dbusername="root";
-$dbpassword="";
-$dbname="facebook";
-$con=mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname);
-?>   
