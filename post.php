af84428c2644e8d2c8545f8a76ba8d254614fe1e
Mayank Sharma <mayanksharma78613@gmail.com>
2019-11-25 07:46:45 +0530

    Delete post.php

--- post.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require 'commonfb.php';
-$post=$_POST['status'];$id=$_SESSION['id'];
-$sql="insert into posts(userid,status,update_date) values('$id','$post',NOW());";
-mysqli_query($con,$sql);
-header('location:main.php');?>
-
-
